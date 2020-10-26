0d8fce69c75286182ce1eab6f6913f798cdecb59
Jake Gadaleta <jake.gads@gmail.com>
2020-10-26 16:03:48 -0400

    added the philly json file

--- flask/app1.py
+++ flask/app1.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import socket
 import ip
 
@@ -11,13 +11,12 @@ def hello():
     global count
     count += 1
     
-    return f"""<h1>Hello You!</h1>
-    <h2>visitor #{count}</h2>
+    return f"""<h1>Hello Visitor #{count}!</h1>
     <h3>connect by going to {ip.ip}:5000</h3>
     <h3>alt-connect: {ip.alt}
     <br><br>
     <ul>
-        <li><a href="/Phillies">Phillies</a></li>
+        <li>"<a href={{ url_for('Phanatic', num='{count}') }}"Phillies</a></li>
         <li><a href="/FLY">Eagles</a></li>
         <li><a href="/DoGgOs">Dogs</a></li>
     </ul> 
@@ -25,7 +24,24 @@ def hello():
 
 app.route("Phillies")
 def Phanatic(num=0):
+    if num == 0:
+        request.args.get('num', 1)
+
     return render_template("template1", title="Phillies", num=num)
+
+app.route("fly")
+def E_A_G_L_E_S(num=0):
+    if num == 0:
+        request.args.get('num', 1)
+
+    return render_template("template2", title="Eagles", num=num)
+
+app.route("doggos")
+def dogs(num=0):
+    if num == 0:
+        request.args.get('num', 1)
+
+    return render_template("template3", title="Dogs", num=num)
 
 
 if __name__ == '__main__':
