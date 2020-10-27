b6257c234b4c1e34a65096e9d852a6a253bd9775
Jake Gadaleta <jake.gads@gmail.com>
2020-10-26 20:40:20 -0400

    added the finishing touches to the dogs

--- flask/app1.py
+++ flask/app1.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import requests
 import json
 import ip
@@ -7,61 +7,59 @@ app = Flask(__name__)
 
 
 count = 0
+
+@app.route("/phils/<num>")
+def phillies(num=1):
+    
+    object = requests.get("https://raw.githubusercontent.com/gadzygadz/Networking_Presentation/main/Data/phillies.json")
+    object = json.loads(object.text)
+    index = (int(num) % len(object)) + 1
+    object = object[str(index)]
+
+    return f"<h1>{index} We in Philly baby</h1>"
+    # return render_template("template1", title="Phillies", num=num, phil=object)
+
+@app.route("/fly/<num>")
+def fly(num=1):
+    if num == 0:
+        num = request.args.get('num', 1)
+
+    object = requests.get("https://raw.githubusercontent.com/gadzygadz/Networking_Presentation/main/Data/eagles.json")
+    object = json.loads(object.text)
+    index = (int(num) % len(object)) + 1
+    object = object[str(index)]
+
+    return render_template("template2", title="Eagles", num=num, eagle=object)
+
+@app.route("/dogs/<num>")
+def dogs(num=1):
+    object = requests.get("https://raw.githubusercontent.com/gadzygadz/Networking_Presentation/main/Data/dogs.json")
+
+    object = json.loads(object.text)
+    index = (int(num) % len(object)) + 1
+    object = object[str(index)]
+
+    return render_template("template3", title="Dogs", num=num, dog=object)
+
+
 @app.route('/')
 def hello():
     global count
-    num = request.args.get('num', 0)
-    if num == 0:
-        count += 1
-        num = count
+    count += 1
+    link = url_for('phillies', num=count)
     
-    return f"""<h1>Hello Visitor #{num}!</h1>
+    return f"""<h1>Hello Visitor #{count}!</h1>
     <h3>connect by going to {ip.ip}:5000</h3>
     <h3>alt-connect: {ip.alt}
     <br><br>
     <ul>
-        <li>"<a href={{ url_for('Phanatic', num='{count}') }}"Phillies</a></li>
-        <li><a href="{{ url_for('E_A_G_L_E_S', num='{count}') }}">Eagles</a></li>
-        <li><a href="{{ url_for('dogs', num='{count}') }}">Dogs</a></li>
+        <li><a href="{url_for('phillies', num=count)}">Phillies</a></li>
+        <li><a href="{url_for('fly', num=count)}">Eagles</a></li>
+        <li><a href="{url_for('phillies', num=count)}">Dogs</a></li>
     </ul> 
     """
 
-app.route("Phillies")
-def Phanatic(num=0):
-    if num == 0:
-        num = request.args.get('num', 1)
-
-    object = json.loads(requests.get("https://raw.githubusercontent.com/gadzygadz/Networking_Presentation/main/Data/phillies.json").text)
-
-    object = object[num % len(object)]
-
-    return render_template("template1", title="Phillies", num=num, phil=object)
-
-app.route("fly")
-def E_A_G_L_E_S(num=0):
-    if num == 0:
-        num = request.args.get('num', 1)
-
-    object = json.loads(requests.get("https://raw.githubusercontent.com/gadzygadz/Networking_Presentation/main/Data/eagles.json").text)
-
-    object = object[num % len(object)]
-
-
-    return render_template("template2", title="Eagles", num=num, eagle=object)
-
-app.route("doggos")
-def dogs(num=0):
-    if num == 0:
-        num = request.args.get('num', 1)
-
-    object = json.loads(requests.get("https://raw.githubusercontent.com/gadzygadz/Networking_Presentation/main/Data/dogs.json").text)
-
-    object = object[num % len(object)]
-
-
-    return render_template("template3", title="Dogs", num=num)
-
-
 if __name__ == '__main__':
+    # app.run()
     app.run(debug=True)
 
