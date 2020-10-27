53defa7e08a2ec98582d634ac7f7f2d99b5d42c4
Jake Gadaleta <jake.gads@gmail.com>
2020-10-27 10:56:24 -0400

    pushing IP

--- flask/const_ip.py
+++ flask/const_ip.py
@@ -3,3 +3,12 @@ import socket
 alt = "Jake you absolute buffoon you forgot to check the Laptop IP address"
 ip = requests.get('https://api.ipify.org').text
 hostname = socket.gethostname()
+
+with open(f"../Data/{hostname}.ip", "w+") as f:
+    f.write(ip)
+
+if hostname == "bluemoon":
+    from os import system
+    system("git add -A")
+    system('git commit -m "pushing IP"')
+    system("git push")
